6b91ec5c3c932016a36eb30f24dc07b29f1a369d
kiarash14 <omidbakhsh22@gmail.com>
2016-03-28 06:42:04 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -90,10 +90,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'shadow' then -- Put everything you like :)
+  if matches[1]:lower() == 'scenemanager' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /shadow ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /scenemanager ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "shadow" then -- Put everything you like :)
+    if matches[2] == "scenemanager" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -142,8 +142,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (shadow)",
-	"^[#!/]([Ss]hadow)"
+    "^[#!/]([Ss]tats) (scenemanager)",
+	"^[#!/]([Ss]cenemanager)"
     }, 
   run = run
 }
